58c49cf0243b4f070263f93af1dbddc83281fc4a
unknown <jules.gioux@gmail.com>
2020-06-04 15:17:12 +0200

    Exercice 2 et 3

---
+++ PHP_Exercice2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 2</title>
+</head>
+<body>
+    <?php 
+    $isEasy = FALSE;
+    if ($isEasy) {
+        print "C'est facile !!";
+    } else {
+        print "C'est difficile !!";
+    };
+    ?>
+</body>
+</html>
---
+++ PHP_Exercice3/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 2</title>
+</head>
+<body>
+    <?php 
+    $age = 18;
+    $gender = "Femme";
+
+    switch (TRUE) {
+        case $age >= 18 && $gender == "Homme" :
+            print ("Vous êtes un homme et vous êtes majeur");
+        break;
+        case $age < 18 && $gender == "Homme" :
+            print ("Vous êtes un homme et vous êtes mineur");
+        break;
+        case $age >= 18 && $gender == "Femme" :
+            print ("Vous êtes une femme et vous êtes majeur");
+        break;
+        case $age < 18 && $gender == "Femme" :
+            print ("Vous êtes une femme et vous êtes mineur");
+        break;
+        default:
+            print ("Il y a une erreur quelque part...");
+            break;
+    }
+
+    ?>
+</body>
+</html>
