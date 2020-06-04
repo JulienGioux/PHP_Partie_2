431a57d4a19654e403571e782e2953f50f411521
unknown <jules.gioux@gmail.com>
2020-06-04 14:56:29 +0200

    Exercice 1

---
+++ PHP_Exercice1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercice 1</title>
+</head>
+<body>
+    <?php 
+    $age = 21;
+    if ($age >= 18) {
+        print "Vous êtes majeur !";
+    } else {
+        print "vous êtes mineur !";
+    };
+    ?>
+</body>
+</html>
